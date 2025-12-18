fde89d21bbcdfc40079ccb6d42574174d0b2fa18
Corey Watson <limehawk@users.noreply.github.com>
2025-12-18 02:56:17 -0500

    Add proxmox-dietpi-installer as submodule

--- scripts/dietpi-install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# DietPi Installer - fetches latest from limehawk/proxmox-dietpi-installer
-# Source: https://github.com/limehawk/proxmox-dietpi-installer
-bash <(curl -fsSL https://raw.githubusercontent.com/limehawk/proxmox-dietpi-installer/main/dietpi-install.sh)
